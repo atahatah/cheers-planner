492fc4fe89d4e4cf6fee8648ffeedab5f1147423
atahatah <ratdotl.hata@gmail.com>
2025-05-25 02:22:13 +0900

    fix: Firebaseのエミュレーター設定を追加し、必要なパッケージをインポートしました

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:cheers_planner/index.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -18,5 +23,11 @@ void main() async {
     // androidProvider: AndroidProvider.debug,
     // appleProvider: AppleProvider.appAttest,
   );
+  if (kDebugMode) {
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8081);
+    await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+    FirebaseFunctions.instance.useFunctionsEmulator('localhost', 5001);
+    await FirebaseStorage.instance.useStorageEmulator('localhost', 9199);
+  }
   runApp(const ProviderScope(child: MyApp()));
 }
