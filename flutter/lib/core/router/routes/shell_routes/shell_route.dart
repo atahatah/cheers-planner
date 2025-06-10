20e5dc61002135f31d6a2c15f4ffc9a8fcf16bfd
石山 和樹 <ratdotl.hata@gmail.com>
2025-06-10 18:46:00 +0900

    feat: responsive navigation

--- flutter/lib/core/router/routes/shell_routes/shell_route.dart
+++ flutter/lib/core/router/routes/shell_routes/shell_route.dart
@@ -32,25 +32,61 @@ class MainShellRouteData extends StatefulShellRouteData {
 
 class ShellScreen extends StatelessWidget {
   const ShellScreen({super.key, required this.navigator});
+
   final StatefulNavigationShell navigator;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: navigator,
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: navigator.currentIndex,
-        onDestinationSelected: (int index) {
-          navigator.goBranch(
-            index,
-            initialLocation: index == navigator.currentIndex,
-          );
-        },
-        destinations: const <Widget>[
-          NavigationDestination(icon: Icon(Icons.add), label: 'Counter'),
-          NavigationDestination(icon: Icon(Icons.chat), label: 'Chat'),
-          NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
-        ],
-      ),
-    );
+    final useRail = MediaQuery.of(context).size.width >= 600;
+    final destinations = const <NavigationDestination>[
+      NavigationDestination(icon: Icon(Icons.add), label: 'Counter'),
+      NavigationDestination(icon: Icon(Icons.chat), label: 'Chat'),
+      NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
+    ];
+
+    void onSelect(int index) {
+      navigator.goBranch(
+        index,
+        initialLocation: index == navigator.currentIndex,
+      );
+    }
+
+    if (useRail) {
+      return Scaffold(
+        body: Row(
+          children: [
+            NavigationRail(
+              selectedIndex: navigator.currentIndex,
+              onDestinationSelected: onSelect,
+              labelType: NavigationRailLabelType.selected,
+              destinations: const <NavigationRailDestination>[
+                NavigationRailDestination(
+                  icon: Icon(Icons.add),
+                  label: Text('Counter'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.chat),
+                  label: Text('Chat'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.settings),
+                  label: Text('Settings'),
+                ),
+              ],
+            ),
+            Expanded(child: navigator),
+          ],
+        ),
+      );
+    } else {
+      return Scaffold(
+        body: navigator,
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: navigator.currentIndex,
+          onDestinationSelected: onSelect,
+          destinations: destinations,
+        ),
+      );
+    }
   }
 }
