01af613fd9e26a66674e640f3fa232011b98f2b8
atahatah <ratdotl.hata@gmail.com>
2025-06-10 21:15:59 +0900

    feat: Snackbar機能を追加し、アプリ全体での通知を改善

---
+++ flutter/lib/core/app/snackbar_repo.g.dart
@@ -0,0 +1,46 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'snackbar_repo.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$snackBarKeyHash() => r'd8f807659efabdad5fe4fd1f92b312bc492ff47b';
+
+/// See also [snackBarKey].
+@ProviderFor(snackBarKey)
+final snackBarKeyProvider =
+    AutoDisposeProvider<GlobalKey<ScaffoldMessengerState>>.internal(
+      snackBarKey,
+      name: r'snackBarKeyProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$snackBarKeyHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SnackBarKeyRef =
+    AutoDisposeProviderRef<GlobalKey<ScaffoldMessengerState>>;
+String _$snackBarRepoHash() => r'38bbdf3c67130dd2aaae624d46818bcd626d72f2';
+
+/// See also [snackBarRepo].
+@ProviderFor(snackBarRepo)
+final snackBarRepoProvider = AutoDisposeProvider<SnackBarRepo>.internal(
+  snackBarRepo,
+  name: r'snackBarRepoProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$snackBarRepoHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SnackBarRepoRef = AutoDisposeProviderRef<SnackBarRepo>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
