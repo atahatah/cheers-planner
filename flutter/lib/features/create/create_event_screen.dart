d61a0242e251985e107bd1bfbb9f3983b0968d00
atahatah <ratdotl.hata@gmail.com>
2025-06-29 16:51:40 +0900

    feat: 候補日時の管理機能を追加し、日付と時間の選択ダイアログを実装

--- flutter/lib/features/create/create_event_screen.dart
+++ flutter/lib/features/create/create_event_screen.dart
@@ -17,7 +17,7 @@ class CreateEventScreen extends HookConsumerWidget {
     final loading = useState(false);
     final eventName = useTextEditingController();
     final deadline = useState<DateTime?>(null);
-    final candidateDateTimes = useState<List<CandidateDateTime>>([]);
+    final candidateDateTimes = useState<List<DateTime>>([]);
     final candidateAreas = useState<List<CandidateArea>>([]);
     final allergiesEtc = useTextEditingController();
     final budgetUpperLimit = useTextEditingController();
@@ -36,9 +36,48 @@ class CreateEventScreen extends HookConsumerWidget {
       }
     }, [context, deadline]);
 
+    final deleteCandidateDateTime = useCallback((DateTime candidateDateTime) {
+      candidateDateTimes.value = List.from(candidateDateTimes.value)
+        ..removeAt(candidateDateTimes.value.indexOf(candidateDateTime));
+    }, [candidateDateTimes]);
+
+    final addCandidateDateTime = useCallback(() async {
+      // 日付と時間を選択するためのダイアログを表示
+      final selectedDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2100),
+      );
+      if (selectedDate == null) {
+        return;
+      }
+      final selectedTime = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.now(),
+      );
+      if (selectedTime == null) {
+        return;
+      }
+      // 選択された日付と時間を結合してDateTimeを作成
+      final candidateDateTime = DateTime(
+        selectedDate.year,
+        selectedDate.month,
+        selectedDate.day,
+        selectedTime.hour,
+        selectedTime.minute,
+      );
+      // 候補日時リストに追加
+      candidateDateTimes.value = [
+        ...candidateDateTimes.value,
+        candidateDateTime,
+      ];
+    }, [context, candidateDateTimes]);
+
     Future<void> submit() async {
       try {
         loading.value = true;
+        final parsedMinutes = int.parse(minutes.text);
         final uid = ref.read(requireUserProvider).uid;
         final id = await ref
             .read(eventEntriesRepoProvider)
@@ -46,14 +85,21 @@ class CreateEventScreen extends HookConsumerWidget {
               EventEntry(
                 purpose: eventName.text,
                 dueDate: deadline.value ?? DateTime.now(),
-                candidateDateTimes: candidateDateTimes.value,
+                candidateDateTimes: candidateDateTimes.value
+                    .map(
+                      (e) => CandidateDateTime(
+                        start: e,
+                        end: e.add(Duration(minutes: parsedMinutes)),
+                      ),
+                    )
+                    .toList(),
                 candidateAreas: candidateAreas.value,
                 allergiesEtc: allergiesEtc.text,
                 organizerId: [uid],
                 participantId: [uid],
                 budgetUpperLimit: int.tryParse(budgetUpperLimit.text) ?? 0,
                 fixedQuestion: fixedQuestion.value,
-                minutes: int.tryParse(minutes.text) ?? 0,
+                minutes: parsedMinutes,
               ),
             );
         ManagementRoute(id).go(context);
@@ -90,13 +136,31 @@ class CreateEventScreen extends HookConsumerWidget {
                       : 'Deadline: ${deadline.value!.toLocal()}',
                 ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  // Logic to add candidate date times
-                  // For example, you can show a dialog to select start and end times
-                  // and then add them to candidateDateTimes.value
+              ValueListenableBuilder(
+                valueListenable: minutes,
+                builder: (context, value, child) {
+                  return Column(
+                    children: [
+                      for (final candidateDateTime in candidateDateTimes.value)
+                        Row(
+                          children: [
+                            Text(
+                              '${candidateDateTime.toLocal()} - ${candidateDateTime.add(Duration(minutes: int.tryParse(minutes.text) ?? 0)).toLocal()}',
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () =>
+                                  deleteCandidateDateTime(candidateDateTime),
+                            ),
+                          ],
+                        ),
+                    ],
+                  );
                 },
-                child: const Text('Add Candidate Date Times'),
+              ),
+              ElevatedButton(
+                onPressed: addCandidateDateTime,
+                child: const Text('日程候補を追加'),
               ),
               ElevatedButton(
                 onPressed: () {
@@ -105,19 +169,6 @@ class CreateEventScreen extends HookConsumerWidget {
                   // and then add them to candidateAreas.value
                 },
                 child: const Text('Add Candidate Areas'),
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: candidateDateTimes.value.length,
-                  itemBuilder: (context, index) {
-                    final candidate = candidateDateTimes.value[index];
-                    return ListTile(
-                      title: Text(
-                        'From ${candidate.start.toLocal()} to ${candidate.end.toLocal()}',
-                      ),
-                    );
-                  },
-                ),
               ),
               Expanded(
                 child: ListView.builder(
