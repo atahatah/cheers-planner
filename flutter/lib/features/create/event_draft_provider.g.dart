ae9ffd8a8775c65a73695177aa2d1d82cad177b0
石山 和樹 <ratdotl.hata@gmail.com>
2025-06-30 09:22:49 +0900

    イベント作成時にGeminiと相談できる機能を追加

---
+++ flutter/lib/features/create/event_draft_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'event_draft_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$eventDraftHash() => r'13b0a3660cb4f868bd9f895d36234cf0a61b22df';
+
+/// See also [EventDraft].
+@ProviderFor(EventDraft)
+final eventDraftProvider =
+    AutoDisposeNotifierProvider<EventDraft, EventEntry>.internal(
+      EventDraft.new,
+      name: r'eventDraftProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$eventDraftHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$EventDraft = AutoDisposeNotifier<EventEntry>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
